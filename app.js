ce91ed990f9cfea2a71431b10373b3a45cbbc8d1
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-26 12:31:19 -0800

    GETTING A WEBPAGE

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("You should put some code here!");
+const request = require('request');
+const jsdom = require("jsdom");
+
+
+request('https://zynh0722.github.io/seip2202-twiddler/', (err, res, body) => {
+  if (err) { return console.log(err); }
+
+  const {JSDOM} = jsdom;
+  const dom = new JSDOM(body);
+  const $ = (require('jquery'))(dom.window);
+
+  console.log($('body').html());
+});
